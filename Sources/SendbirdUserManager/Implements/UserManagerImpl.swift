da5d3fee7441fe96be97f36c47138bda29726481
eastjohn <eastjohn@gmail.com>
2024-08-31 23:43:32 +0900

    UserManagerImpl 기본 구현체 추가

--- Sources/SendbirdUserManager/Implements/UserManagerImpl.swift
+++ Sources/SendbirdUserManager/Implements/UserManagerImpl.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+final class UserManagerImpl: SBUserManager {
+    var networkClient: SBNetworkClient = NetworkClientImpl()
+    var userStorage: SBUserStorage = UserStorageImpl()
+
+    func initApplication(applicationId: String, apiToken: String) {
+    }
+
+    func createUser(params: UserCreationParams, completionHandler: ((UserResult) -> Void)?) {
+    }
+
+    func createUsers(params: [UserCreationParams], completionHandler: ((UsersResult) -> Void)?) {
+    }
+
+    func updateUser(params: UserUpdateParams, completionHandler: ((UserResult) -> Void)?) {
+    }
+
+    func getUser(userId: String, completionHandler: ((UserResult) -> Void)?) {
+    }
+
+    func getUsers(nicknameMatches: String, completionHandler: ((UsersResult) -> Void)?) {
+    }
+}
