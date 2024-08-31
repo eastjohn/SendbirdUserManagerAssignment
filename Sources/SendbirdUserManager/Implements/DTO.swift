e67bd0bf3108b94b6718c9a6ad1e1d45c3bb0c70
eastjohn <eastjohn@gmail.com>
2024-09-01 00:10:24 +0900

    UpdateUserRequest 추가

--- Sources/SendbirdUserManager/Implements/DTO.swift
+++ Sources/SendbirdUserManager/Implements/DTO.swift
@@ -84,3 +84,41 @@ struct CreateUserRequest: DTOModel, Encodable {
         return try? decoder.decode(UserResponse.self, from: data) as? Response
     }
 }
+
+struct UpdateUserRequest: DTOModel {
+    typealias Response = UpdateUserResponse
+
+    let userId: String
+    let nickname: String?
+    let profileUrl: String?
+
+    struct UpdateUserResponse: Decodable {
+        let userId: String
+        let nickname: String
+        let profileUrl: String?
+    }
+
+    func makeURLRequest(url: URL, headers: [String: String]) -> URLRequest? {
+        var urlRequest = URLRequest(url: url.appendingPathComponent(APIConstants.updateUserEndPoint.replacingOccurrences(of: "{user_id}", with: userId)))
+        urlRequest.httpMethod = Method.put.rawValue
+        var body: [String: String] = [:]
+        if let nickname {
+            body["nickname"] = nickname
+        }
+        if let profileUrl {
+            body["profileUrl"] = profileUrl
+        }
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: body) else { return nil }
+        urlRequest.httpBody = httpBody
+        headers.forEach { key, value in
+            urlRequest.addValue(value, forHTTPHeaderField: key)
+        }
+        return urlRequest
+    }
+
+    func makeResponse<Response>(data: Data) -> Response? {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        return try? decoder.decode(UpdateUserResponse.self, from: data) as? Response
+    }
+}
