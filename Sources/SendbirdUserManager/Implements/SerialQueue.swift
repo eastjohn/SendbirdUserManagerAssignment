7f49ff8006dbc8fed7639e98a1dd039c68c75601
eastjohn <eastjohn@gmail.com>
2024-09-01 00:49:36 +0900

    SerialQueue 구현

--- Sources/SendbirdUserManager/Implements/SerialQueue.swift
+++ Sources/SendbirdUserManager/Implements/SerialQueue.swift
@@ -6,3 +6,53 @@
 //
 
 import Foundation
+
+struct SerialQueue {
+    private static let queueNameKey = DispatchSpecificKey<String>()
+    private let queue: DispatchQueue
+
+    init(label: String, qos: DispatchQoS = .userInitiated) {
+        let queue = DispatchQueue(label: label, qos: qos)
+        queue.setSpecific(key: Self.queueNameKey, value: label)
+        self.queue = queue
+    }
+
+    func sync(execute: () -> Void) {
+        if isCurrentRunQueue() {
+            execute()
+        } else {
+            queue.sync {
+                execute()
+            }
+        }
+    }
+
+    func run(execute: @escaping () -> Void) {
+        if isCurrentRunQueue() {
+            execute()
+        } else {
+            queue.async {
+                execute()
+            }
+        }
+    }
+
+    func asyncAfter(deadline: DispatchTime, execute: @escaping () -> Void) {
+        queue.asyncAfter(deadline: deadline, execute: execute)
+    }
+
+    func preconditionOnQueue() {
+        dispatchPrecondition(condition: .onQueue(queue))
+    }
+
+    func preconditionNotOnQueue() {
+        dispatchPrecondition(condition: .notOnQueue(queue))
+    }
+}
+
+extension SerialQueue {
+    private func isCurrentRunQueue() -> Bool {
+        guard let specificValue = queue.getSpecific(key: Self.queueNameKey) else { return false }
+        return DispatchQueue.getSpecific(key: Self.queueNameKey) == specificValue
+    }
+}
