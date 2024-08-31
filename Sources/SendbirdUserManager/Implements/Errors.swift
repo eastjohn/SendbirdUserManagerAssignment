6b1c4b38090a0b94046dc41d3a0926b129ab8fbe
eastjohn <eastjohn@gmail.com>
2024-09-01 00:45:19 +0900

    SBUserManagerError 추가

--- Sources/SendbirdUserManager/Implements/Errors.swift
+++ Sources/SendbirdUserManager/Implements/Errors.swift
@@ -39,3 +39,56 @@ extension SBNetworkClientError: LocalizedError {
         }
     }
 }
+
+enum SBUserManagerError: Error {
+    case nilSelf
+    case notInitialized
+    case invalidSession
+    case userIdLengthExceeded
+    case nicknameLengthExceeded
+    case profileUrlLengthExcceded
+    case emptyUserId
+    case emptyNickname
+    case alreadyExistUser
+    case creatingUsersExceeded
+    case failedContinueCreateUsers(createdUsers: [SBUser], failedUsers: [(user: SBUser, reason: Error)])
+}
+
+extension SBUserManagerError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .nilSelf:
+            "The object has been destroyed."
+
+        case .notInitialized:
+            "Not initialized."
+
+        case .invalidSession:
+            "The session is invalid."
+
+        case .userIdLengthExceeded:
+            "The userId exceeded the limited length."
+
+        case .nicknameLengthExceeded:
+            "The nickname exceeded the limited length."
+
+        case .profileUrlLengthExcceded:
+            "The profile url exceeded the limited length."
+
+        case .emptyUserId:
+            "The userId is an empty string."
+
+        case .emptyNickname:
+            "The nickname is an empty string."
+
+        case .alreadyExistUser:
+            "The user already exists."
+
+        case .creatingUsersExceeded:
+            "The number of users you tried to create exceeded the limit."
+
+        case let .failedContinueCreateUsers(createdUsers, failedUsers):
+            "Partially completed user creation. (Success: \(createdUsers.count), Failure: \(failedUsers.count)"
+        }
+    }
+}
