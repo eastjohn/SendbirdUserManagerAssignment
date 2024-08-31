97f902d41fb7034be250fd386a1b111692675d60
eastjohn <eastjohn@gmail.com>
2024-09-01 00:51:40 +0900

    initApplication method 구현

--- Sources/SendbirdUserManager/Implements/UserManagerImpl.swift
+++ Sources/SendbirdUserManager/Implements/UserManagerImpl.swift
@@ -8,10 +8,24 @@
 import Foundation
 
 final class UserManagerImpl: SBUserManager {
-    var networkClient: SBNetworkClient = NetworkClientImpl()
-    var userStorage: SBUserStorage = UserStorageImpl()
+    private var session: Session?
+    private let queue = SerialQueue(label: "com.userManager.serialQueue")
+    private let internalNetworkClient = NetworkClientImpl()
+    private let internalUserStorage = UserStorageImpl()
+
+    var networkClient: SBNetworkClient { internalNetworkClient }
+    var userStorage: SBUserStorage { internalUserStorage }
 
     func initApplication(applicationId: String, apiToken: String) {
+        // initApplication은 completionHandler가 없어서 비동기가 아닌 sync로 처리.
+        queue.sync {
+            if shouldClearStorage(applicationId: applicationId) {
+                internalUserStorage.clear()
+            }
+            let session = Session(applicationId: applicationId, apiToken: apiToken)
+            internalNetworkClient.initialize(session: session)
+            self.session = session
+        }
     }
 
     func createUser(params: UserCreationParams, completionHandler: ((UserResult) -> Void)?) {
@@ -27,5 +41,12 @@ final class UserManagerImpl: SBUserManager {
     }
 
     func getUsers(nicknameMatches: String, completionHandler: ((UsersResult) -> Void)?) {
+    }
+}
+
+extension UserManagerImpl {
+    private func shouldClearStorage(applicationId: String) -> Bool {
+        queue.preconditionOnQueue()
+        return session?.applicationId != applicationId
     }
 }
