710a65021ffc3ca5e4cd98bea183900a2fe78c82
eastjohn <eastjohn@gmail.com>
2024-08-31 23:48:57 +0900

    Mock객체를 실제 구현체로 변경

--- Tests/SendbirdUserManagerTests/SendbirdUserManagerTests.swift
+++ Tests/SendbirdUserManagerTests/SendbirdUserManagerTests.swift
@@ -10,13 +10,13 @@ import XCTest
 
 final class UserManagerTests: UserManagerBaseTests {
     override func userManager() -> SBUserManager {
-        MockUserManager()
+        UserManagerImpl()
     }
 }
 
 final class UserStorageTests: UserStorageBaseTests {
     override func userStorage() -> SBUserStorage? {
-        MockUserStorage()
+        UserStorageImpl()
     }
 }
 
