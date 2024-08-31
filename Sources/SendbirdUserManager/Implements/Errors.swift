6a9a8c7fe60e76b22b1dd0ad62680503e529f80e
eastjohn <eastjohn@gmail.com>
2024-08-31 23:38:24 +0900

    SBNetworkClientError 추가

--- Sources/SendbirdUserManager/Implements/Errors.swift
+++ Sources/SendbirdUserManager/Implements/Errors.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+enum SBNetworkClientError: Error {
+    case networkError(description: String)
+    case notInitialized
+    case failedToEncoding
+    case failedToDecoding
+    case notImplementResponsible
+    case rateLimitExceeded
+}
+
+extension SBNetworkClientError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .networkError(description: let description):
+            "networkError: \(description)"
+
+        case .notInitialized:
+            "notInitialized"
+
+        case .failedToEncoding:
+            ".failedToEncoding"
+
+        case .failedToDecoding:
+            ".failedToDecoding"
+
+        case .notImplementResponsible:
+            ".notImplementResponsible"
+
+        case .rateLimitExceeded:
+            ".rateLimitExceeded"
+        }
+    }
+}
